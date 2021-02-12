deae5895d65595858994819ff60f8de55ce46da6
Ravi <jravi248@gmail.com>
2021-02-12 20:06:58 +0530

    Added ability to search forward and backward

--- kilo.c
+++ kilo.c
@@ -444,16 +444,35 @@ void editorSave() {
 /*** search ***/
 
 void editorFindCallback(char *query, int key) {
-	if (key == '\r' || key == '\x1b')
-		return;
+	static int last_match = -1;
+	static int direction = 1;
 
+	if (key == '\r' || key == '\x1b') {
+		last_match = -1;
+		direction = 1;
+		return;
+	} else if (key == CURSOR_RIGHT || key == CURSOR_DOWN) {
+		direction = 1;
+	} else if (key == CURSOR_LEFT || key == CURSOR_UP) {
+		direction = -1;
+	} else {
+		last_match = -1;
+		direction = 1;
+	}
+
+	int current = last_match;
 	int i;
 	for (i=0; i<E.numrows; i++) {
-		erow *row = &E.row[i];
+		current += direction;
+		if (current == -1) current = E.numrows - 1;
+		else if (current == E.numrows) current = 0;
+
+		erow *row = &E.row[current];
 		char *match = strstr(row->render, query);
 
 		if (match) {
-			E.cy = i;
+			last_match = current;
+			E.cy = current;
 			E.cx = editorRxToCx(row, match - row->render);
 			E.rowoff = E.numrows;
 			break;
@@ -466,7 +485,7 @@ void editorFind() {
 	int saved_cy = E.cy;
 	int saved_coloff = E.coloff;
 	int saved_rowoff = E.rowoff;
-	char *query = editorPrompt("Search: %s {ESC to cancel)", editorFindCallback);
+	char *query = editorPrompt("Search: %s {ESC|ARROWS|ENTER)", editorFindCallback);
 
 	if (query)
 		free(query);
